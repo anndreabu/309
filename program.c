2acf1554588c34f5d6a2bb6e272331d19804ab85
anndreabu <0310923@teslabg.edu.rs>
2026-04-08 12:26:21 +0200

    Update program.c
    Izmenila sam granu dodavanjem printf f-je

--- program.c
+++ program.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main(){
   printf("Dobro dosli na povezivivanje sa Git hubom\n");
-return 0;
+  printf("Dobro dosli na povezivivanje sa Git hubom\n");
+  printf("Dobro dosli na povezivivanje sa Git hubom\n");
 }
