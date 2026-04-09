bb39aee5c645be2fdcf86960f71378370d5dfeca
anndreabu <0310923@teslabg.edu.rs>
2026-04-09 12:13:04 +0200

    Update program.c

--- program.c
+++ program.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 int main(){
   printf("Dobro dosli na povezivivanje sa Git hubom\n");
+  printf("Dobro dosli na povezivivanje sa Git hubom\n");
+  printf("Dobro dosli na povezivivanje sa Git hubom\n");
+  printf("Dobro dosli na povezivivanje sa Git hubom\n");
 return 0;
 }
